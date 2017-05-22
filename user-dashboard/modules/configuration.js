64ea444ca8ffb2812e615b924f9f9ecc275fae72
lixucheng <lixucheng@aliyun.com>
2017-05-22 09:45:44 +0800

    [CE-33] add document and Dockerfile for user dashboard
    Change-Id: If4a1b5d593ef90fe4f17d4e5d7157fa7437c5cb0
    Signed-off-by: lixucheng <lixucheng@aliyun.com>

--- user-dashboard/modules/configuration.js
+++ user-dashboard/modules/configuration.js
@@ -14,7 +14,7 @@ module.exports = {
         ip: "127.0.0.1",
         port: 27017,
         name: "bc_dashboard",
-        auth: true,
+        auth: false,
         username: "admin",
         password: "passw0rd"
     },
