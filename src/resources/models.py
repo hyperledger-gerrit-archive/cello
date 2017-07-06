7f1e1ff5b3cb7e373d71e8db2ce60c46581a6129
Haitao Yue <hightall@me.com>
2017-07-06 23:38:59 +0800

    [CE-71] Add user authentication for react theme #close
    Support user login for react theme
    Remove auth in nginx conf
    Add login page for react theme
    
    Change-Id: I17ad143766ccf37a70df1d1f76905b998c0d6021
    Signed-off-by: Haitao Yue <hightall@me.com>

---
+++ src/resources/models.py
@@ -0,0 +1,14 @@
+import sys
+import os
+import datetime
+from mongoengine import *
+
+sys.path.append(os.path.join(os.path.dirname(__file__), '..', '..'))
+
+
+class User(Document):
+    username = StringField(unique=True)
+    password = StringField(default=True)
+    active = BooleanField(default=True)
+    isAdmin = BooleanField(default=False)
+    timestamp = DateTimeField(default=datetime.datetime.now())
