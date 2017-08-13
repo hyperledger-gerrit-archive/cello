700f8002bfcac0ea2b731de88c360c69a42662b5
Nikhil Chawla <chawlanikhil24@gmail.com>
2017-08-13 13:52:04 +0530

    [CE-96] Debugging Scripts and manifests
    Added Kube-Dns settings and Nginx conf files,
    which will create a proxy for Kube-Dns which
    later can be added to Docker-DNS to support
    chaincode-container to peer-pod communication.
    
    Signed-off-by: Nikhil Chawla <chawlanikhil24@gmail.com>
    
    Change-Id: I754c47a8e65e3fbe0c8907efc0254e1c9f9e0e3e
    Signed-off-by: Nikhil Chawla <chawlanikhil24@gmail.com>

---
+++ src/agent/k8s/run.sh
@@ -0,0 +1,12 @@
+#Exposing the Kube-DNS to NodePort 30053
+kubectl apply -f manifests/Kube-DNS/kube-dns-nodeport.yaml
+
+#GETTING THE NGINX STREAM PROXY READY TO FORWARD THE INCOMING UDP REQUESTS
+#AT PORT 53 TO Kube-DNS NodePort 30053
+sudo cp nginx/nginx.conf /etc/nginx/nginx.conf
+
+echo "Kube-DNS  and Nginx Proxy are all set"
+
+echo "Now Deploying the Fabric-1.0 cluster"
+
+bash deploy.sh
