4460ad8acb3f8a38a454fa7d0de0a60f0a0a5cd7
Haitao Yue <hightall@me.com>
2017-08-18 17:09:13 +0800

    [CE-112] Refine code structure for user module
    Use flask-restful library to refine code structure
    Move js script to single js file
    Change API base config for user dashboard
    
    Change-Id: Ib6241954f372e27eab9fa50aa407a6afc3b3943f
    Signed-off-by: Haitao Yue <hightall@me.com>

---
+++ src/modules/user/__init__.py
@@ -0,0 +1,1 @@
+from .management import ListUser, CreateUser, UpdateUser, DeleteUser
---
+++ src/modules/user/management/__init__.py
@@ -0,0 +1,4 @@
+from .list import ListUser
+from .create import CreateUser
+from .update import UpdateUser
+from .delete import DeleteUser
