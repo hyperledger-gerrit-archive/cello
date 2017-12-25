184d109a6b600c1d89fe780f8f914712b1280ea4
Haitao Yue <hightall@me.com>
2017-12-25 23:33:18 +0800

    [CE-207] Add chain view page in user dashboard
    Change-Id: Iee79dc5ee58c998af8c318ddb4d0265d273f1ffd
    Signed-off-by: Haitao Yue <hightall@me.com>

---
+++ user-dashboard/js/dashboard/src/components/Charts/index.js
@@ -0,0 +1,7 @@
+import ChartCard from './ChartCard';
+
+const yuan = val => `&yen; ${numeral(val).format('0,0')}`;
+
+export default {
+  ChartCard,
+};
---
+++ user-dashboard/js/dashboard/src/routes/Chain/components/index.js
@@ -0,0 +1,6 @@
+import EmptyView from './emptyView'
+// import ChainModal from './chainModal'
+import ChainView from './chainView'
+import EditModal from './editModal'
+
+export {EmptyView, ChainView, EditModal}
