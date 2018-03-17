52c7043391d300d0faf7c1a7ccbe7c241e6c44cf
Chris Lim <yopep@yahoo.com>
2018-03-17 20:01:08 +0800

    [CE-306] Reduce MongoDB docker image
    - Used the official mongodb 3.4 Dockerfile
      from https://github.com/docker-library/mongo
    - Removed mongodb scripts from the base image
      which reduces the size of the other images to around 40% less
    
    Change-Id: I363a55c07eba21a97560b181113fd2a2006a4c5b
    Signed-off-by: Chris Lim <yopep@yahoo.com>

--- dockerhub/latest/baseimage/install.sh
+++ dockerhub/latest/baseimage/install.sh
@@ -4,15 +4,6 @@ set -x
 # Copyright IBM Corp. All Rights Reserved.
 #
 # SPDX-License-Identifier: Apache-2.0
-#
-# Based thie file on https://github.com/docker-library/mongo/blob/master/3.4/Dockerfile &
-# https://docs.mongodb.com/manual/tutorial/install-mongodb-enterprise-on-ubuntu/#install-mongodb-enterprise
-
-# ----------------------------------------------------------------
-# Install mongo
-# ----------------------------------------------------------------
-
-groupadd -r mongodb && useradd -r -g mongodb mongodb
 
 apt-get update \
 && apt-get install -y --no-install-recommends ca-certificates jq numactl sudo\
@@ -33,31 +24,11 @@ set -x \
 	&& gosu nobody true
 
 mkdir /docker-entrypoint-initdb.d
-
 # Add GPG Keys & update apt sources
 
 apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
 
-echo "deb [ arch=amd64,arm64,ppc64el,s390x ] http://repo.mongodb.com/apt/ubuntu xenial/mongodb-enterprise/3.4 multiverse" | tee /etc/apt/sources.list.d/mongodb-enterprise.list
-
 apt-get update
-
-export MONGO_PACKAGE=mongodb-enterprise
-# export MONGO_REPO=repo.mongodb.com
-# export MONGO_PACKAGE=${MONGO_PACKAGE} MONGO_REPO=${MONGO_REPO}
-
-export MONGO_MAJOR=3.4
-export MONGO_VERSION=3.4.10
-
-apt-get install -y \
-		${MONGO_PACKAGE}=$MONGO_VERSION \
-		${MONGO_PACKAGE}-server=$MONGO_VERSION \
-		${MONGO_PACKAGE}-shell=$MONGO_VERSION \
-		${MONGO_PACKAGE}-mongos=$MONGO_VERSION \
-		${MONGO_PACKAGE}-tools=$MONGO_VERSION
-
-mkdir -p /data/db /data/configdb \
-&& chown -R mongodb:mongodb /data/db /data/configdb
 
 # ----------------------------------------------------------------
 # Install NodeJS
