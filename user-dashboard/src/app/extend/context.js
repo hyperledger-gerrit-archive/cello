4c6db17ace29588d1f0a89e6e79fe4da490e5dd5
Haitao Yue <hightall@me.com>
2018-06-19 15:06:45 +0800

    [CE-377] Support initialize fabric in user dashboard
    When user apply chain in user dashboard,
    will create,join default channel named mychannel.
    Can download network-config.json file through user dashboard.
    
    Change-Id: Id29a6a8241f3928af356f34e8907e33e6518d848
    Signed-off-by: Haitao Yue <hightall@me.com>

---
+++ user-dashboard/src/app/extend/context.js
@@ -0,0 +1,25 @@
+'use strict';
+
+module.exports = {
+  get helper() {
+    return this.app.fabricHelper;
+  },
+  get getClientForOrg() {
+    return this.app.getClientForOrg;
+  },
+  get getOrgAdmin() {
+    return this.app.getOrgAdmin;
+  },
+  get getChannelForOrg() {
+    return this.app.getChannelForOrg;
+  },
+  get createChannel() {
+    return this.app.createChannel;
+  },
+  get joinChannel() {
+    return this.app.joinChannel;
+  },
+  get sleep() {
+    return this.app.sleep;
+  },
+};
